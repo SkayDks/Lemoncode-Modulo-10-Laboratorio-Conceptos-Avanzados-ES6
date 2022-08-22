e32436c0e25dc6369dc97b2d37c4a523d68706f2
Tony <shidadk@hotmail.es>
2022-08-22 18:41:03 +0200

    retoques finales

--- app.js
+++ app.js
@@ -1,122 +1,162 @@
 // Ejercicios Entregables
 // Estan todas las funciones descomentadas por que alguna funcion es usada por otra aprovechando ya por si la utilidad que proporcionan
 
-// --- hasId ---
-// Implementa una función llamada hasId que admita como parámetro un objeto y compruebe si dicho objeto tiene una propiedad llamada id (debe devolver booleano true/false).
-const hasId = (obj) => "id" in obj;
-let objHasId = {  name: "papas fritas", type: "vegetal" };
+/** --- hasId ---
+ *  Implementa una función llamada hasId que admita como parámetro un objeto y
+ *  compruebe si dicho objeto tiene una propiedad llamada id (debe devolver booleano true/false).
+ *  cree 2 funciones distintas, una usando el in que aun el objeto pasado contenga un id
+ *  con informacion de 0, -0, undefined, null, false, "", o NaN devolvera true, solo devolvera false
+ *  si no existe una propiedad id
+ *  la segunda funcion si exite un id pero contiene 0, -0, undefined, null, false, "", o NaN devolvera false
+ */
+
+const hasIdExt = (obj) => "id" in obj;
+const hasId = ({ id }) => Boolean(id);
+let objHasId = { id: undefined, name: "papas fritas", type: "vegetal" };
 let modObjHasId = hasId(objHasId);
-console.log(objHasId);
-console.log(modObjHasId);
+let modObjHasIdExt = hasIdExt(objHasId);
+console.log("Objeto base", objHasId);
+console.log(
+  "Resultado hasId:",
+  modObjHasId,
+  "Resultado hasIdExt:",
+  modObjHasIdExt
+);
 
-// --- head ---
+/** --- head --- */
+
 const head = ([a, ...b]) => a;
-// let arrayHead = ["atun", "beicon", "cacauetes", "doritos"];
-// let modArrayHead = head(arrayHead);
-// console.log(arrayHead);
-// console.log(modArrayHead);
+let arrayHead = ["atun", "beicon", "Croquetas", "doritos"];
+let modArrayHead = head(arrayHead);
+console.log("Objeto base", arrayHead);
+console.log("Resultado head:", modArrayHead);
 
-// --- tail ---
+/** --- tail --- */
+
 const tail = ([, ...a]) => [...a];
-// let arrayTail = ["a", "b", "c", "d"];
-// let modArrayTail = tail(arrayTail);
-// console.log(arrayTail);
-// console.log(modArrayTail);
+let arrayTail = ["a", "b", "c", "d"];
+let modArrayTail = tail(arrayTail);
+console.log("Objeto base", arrayTail);
+console.log("Resultado tail:", modArrayTail);
 
-// --- swapFirstToLast ---
+/** --- swapFirstToLast --- */
+
 const swapFirstToLast = ([a, ...b]) => [...b, a];
-// let arraySwapFirstToLast = ["atun", "beicon", "cacauetes", "doritos"];
-// let modArraySwapFirstToLast = swapFirstToLast(arraySwapFirstToLast);
-// console.log(arraySwapFirstToLast);
-// console.log(modArraySwapFirstToLast);
+let arraySwapFirstToLast = ["atun", "beicon", "Croquetas", "doritos"];
+let modArraySwapFirstToLast = swapFirstToLast(arraySwapFirstToLast);
+console.log("Objeto base", arraySwapFirstToLast);
+console.log("Resultado swapFirstToLast:", modArraySwapFirstToLast);
 
-// --- excludeId ---
+/** --- excludeId --- */
 const exlcudeId = ({ id, ...rest }) => ({ ...rest });
-// let objExcludeId = { id: 1, name: "papas fritas", type: "vegetal" };
-// let modObjExcludeId = exlcudeId(objExcludeId);
-// console.log(objExcludeId);
-// console.log(modObjExcludeId);
+let objExcludeId = { id: 1, name: "papas fritas", type: "vegetal" };
+let modObjExcludeId = exlcudeId(objExcludeId);
+console.log("Objeto base", objExcludeId);
+console.log("Resultado excludeId:", modObjExcludeId);
 
-// --- wordsStartingWithA ---
-// he aprovechado la funcion head generada mas arriba para sacar la primera letra (comprove que seria tanto para arrais como para strings)
+/** --- wordsStartingWithA ---
+ * he aprovechado la funcion head generada mas arriba para sacar la primera letra (comprove que seria tanto para arrais como para strings)
+ */
 const wordsStartingWithA = (array) =>
   array.filter((words) => head(words).toLowerCase() === "a");
-// let arrayWordsStartingWhitA = ["atun", "beicon", "cacauetes", "doritos", "Albaricoque", "Banana"];
-// let modArrayWordsStartingWhitA = wordsStartingWithA(arrayWordsStartingWhitA);
-// console.log(arrayWordsStartingWhitA);
-// console.log(modArrayWordsStartingWhitA);
+let arrayWordsStartingWhitA = [
+  "atun",
+  "beicon",
+  "Croquetas",
+  "doritos",
+  "Albaricoque",
+  "Banana",
+];
+let modArrayWordsStartingWhitA = wordsStartingWithA(arrayWordsStartingWhitA);
+console.log("Objeto base", arrayWordsStartingWhitA);
+console.log("Resultado wordsStartingWithA:", modArrayWordsStartingWhitA);
 
-// --- concat ---
+/** --- concat --- */
 const concat = (...a) => a.join(" | ");
-// let exampleConcat = concat("atun", "beicon", "cacauetes", "doritos", "Albaricoque", "Banana")
-// console.log(exampleConcat);
+let exampleConcat = concat(
+  "atun",
+  "beicon",
+  "Croquetas",
+  "doritos",
+  "Albaricoque",
+  "Banana"
+);
+console.log("Resultado concat:", exampleConcat);
 
-// --- multArray ---
+/** --- multArray --- */
 const multArray = (mult, [...num]) => num.map((num) => num * mult);
-// let arrayMultArray = [2, 4, 6, 1, 3, 2];
-// let modArrayMultArray = multArray(2, arrayMultArray);
-// console.log(arrayMultArray);
-// console.log(modArrayMultArray);
+let arrayMultArray = [2, 4, 6, 1, 3, 2];
+let modArrayMultArray = multArray(2, arrayMultArray);
+console.log("Objeto base", arrayMultArray);
+console.log("Resultado multArray:", modArrayMultArray);
 
-// --- calcMult ---
+/** --- calcMult --- */
 const calcMult = (...num) => num.map((num) => num * num);
-// let exampleCalcMult = calcMult(2,3,7,4,2);
-// console.log(exampleCalcMult);
+let exampleCalcMult = calcMult(2, 3, 7, 4, 2);
+console.log("Resultado calcMult:", exampleCalcMult);
 
 //  --- Ejercicios Extra ---
-// --- swapFirstSecond ---
+/** --- swapFirstSecond --- */
 const swapFirstSecond = ([a, b, ...rest]) => [b, a, ...rest];
-// let arraySwapFirstSecond = ["atun", "beicon", "cacauetes", "doritos", "Banana"];
-// let modArraySwapFirstSecond = swapFirstSecond(arraySwapFirstSecond);
-// console.log(arraySwapFirstSecond);
-// console.log(modArraySwapFirstSecond);
+let arraySwapFirstSecond = ["atun", "beicon", "Croquetas", "doritos", "Banana"];
+let modArraySwapFirstSecond = swapFirstSecond(arraySwapFirstSecond);
+console.log("Objeto base", arraySwapFirstSecond);
+console.log("Resultado swapFirstSecond:", modArraySwapFirstSecond);
 
-// --- firstEqual ---
+/** --- firstEqual --- */
 // use la funcion head de mas arriba
-const firstEqual =(char, ...str) => str.every(str => head(str)===char); 
-// let exampleFirstEqual = firstEqual("a", "atun", "abeicon", "acacauetes", "adoritos", "aAlbaricoque", "aBanana")
-// console.log(exampleFirstEqual);
+const firstEqual = (char, ...str) => str.every((str) => head(str) === char);
+let exampleFirstEqual1 = firstEqual("a", "atun", "abeicon", "aCroquetas");
+let exampleFirstEqual2 = firstEqual("a", "atun", "beicon", "Croquetas");
 
+console.log("Resultado firstEqual con a de inicio:", exampleFirstEqual1);
+console.log("Resultado firstEqual sin a de inicio:", exampleFirstEqual2);
 
-// --- longest ---
-// Implementa una función longest que admita múltiples arrays como entrada, y devuelva el array más largo.
-const longest = (...arrays) => arrays.map((array) => array.length);
-let exampleLongest = longest([1, 2, 3], [23], ["a", "B", "f", "h"]);
-// TIP: No utilices bucles. Hay diversas formas de hacerlo.
+/** --- longest --- */
+const longest = (...arrays) =>
+  arrays.reduce((a, b) => (a = a.length > b.length ? a : b));
+let arrayLongest1 = [1, 2, 3, 4],
+  arrayLongest2 = [23],
+  arrayLongest3 = ["a", "B", "f", "h"];
+let exampleLongest = longest(arrayLongest1, arrayLongest2, arrayLongest3);
+console.log("Arrays base: ", arrayLongest1, arrayLongest2, arrayLongest3);
+console.log("Resultado longest:", exampleLongest);
 
-// --- searchInStringV1 ---
+/** --- searchInStringV1 --- */
 const searchInStringV1 = (str, char) =>
   Array.from(str).reduce((acc, strChar) => acc + (strChar === char ? 1 : 0), 0);
-// let exampleSearchInStringV1 = searchInStringV1("papas fritas", "a");
-// console.log(exampleSearchInStringV1);
+let exampleSearchInStringV1 = searchInStringV1("papas fritas", "a");
+console.log("Resultado searchInStringV1:", exampleSearchInStringV1);
 
-// --- searchInStringV2 ---
+/** --- searchInStringV2 --- */
 const searchInStringV2 = (str, char) =>
   Array.from(str)
     .map((strChar) => strChar === char)
     .filter((str) => str).length;
-// let exampleSearchInStringV2 = searchInStringV2("papas fritas", "a");
-// console.log(exampleSearchInStringV2);
+let exampleSearchInStringV2 = searchInStringV2("papas fritas", "a");
+console.log("Resultado searchInStringV2:", exampleSearchInStringV2);
 
-// --- sortCharacters ---
-const sortCharacters = (str) => Array.from(str).sort();
-// let exampleSortCharacters = sortCharacters("papas fritas").join("");
-// console.log(exampleSortCharacters);
+/** --- sortCharacters --- */
+const sortCharacters = (str) => Array.from(str).sort().join("");
+let strSortCharacters = "papas fritas";
+let modStrSortCharacters = sortCharacters(strSortCharacters);
+console.log("String base:", strSortCharacters);
+console.log("Resultado sortCharacters:", modStrSortCharacters);
 
-// --- shout ---
+/** --- shout --- */
 const shout = (...str) => str.map((str) => str.toUpperCase() + "!").join(", ");
-// let exampleShout = shout(
-//   "atun",
-//   "beicon",
-//   "cacauetes",
-//   "doritos",
-//   "Albaricoque",
-//   "Banana"
-// );
-// console.log(exampleShout);
+let exampleShout = shout(
+  "atun",
+  "beicon",
+  "Croquetas",
+  "doritos",
+  "Albaricoque",
+  "Banana"
+);
+console.log("Resultado shout:", exampleShout);
 
 // --- Lista de la compra ---
-// Dada la siguiente lista de la compra: 
+// Dada la siguiente lista de la compra:
 const shoppingCart = [
   { category: "Frutas y Verduras", product: "Lechuga", price: 0.8, units: 1 },
   {
@@ -138,22 +178,28 @@ const shoppingCart = [
 ];
 // A. Obtén una nueva lista donde aparezca el IVA (21%) de cada producto.
 const addIVA = (obj) => obj.map((obj) => ({ ...obj, iva: 21 }));
-// console.log(addIVA(shoppingCart));
+console.log("Resultado addIVA:", addIVA(shoppingCart));
 
 // B. Ordena la lista de más a menos unidades.
 const orderList = (obj) => obj.sort((a, b) => b.units - a.units);
-//console.log(orderList(shoppingCart));
+//Copie el array antes de usar la funcion para que no modificara el array original
+let sortShoppingCart =  [...shoppingCart];
+console.log("Resultado orderList:", orderList(sortShoppingCart));
+
 
 // C. Obtén el subtotal gastado en droguería.
 const subtotalDrugstore = (obj) =>
   obj
     .filter((item) => item.category === "Droguería")
     .reduce((acc, item) => acc + item.price * item.units, 0);
-// console.log(subtotalDrugstore(shoppingCart));
+console.log("Resultado subtotalDrugstore:", subtotalDrugstore(shoppingCart));
 
 // D. Obtén la lista por consola en formato "Producto -> Precio Total €" y ordenada por categoría.
 const printProductoTotalPrice = (obj) =>
   obj
     .sort((a, b) => a.category.localeCompare(b.category))
     .map((obj) => console.log(`"${obj.product} -> ${obj.price * obj.units}€"`));
-//console.log(printProductoTotalPrice(shoppingCart));
+
+//Copie el array antes de usar la funcion para que no modificara el array original
+let printShoppingCart =  [...shoppingCart];
+printProductoTotalPrice(printShoppingCart);
